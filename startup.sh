beb2449771559b8b4d89dc07376d876c995c0001
Luc1007 <lucio010720030621@gmail.com>
2024-05-31 13:15:16 -0600

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+
+# Activar el entorno virtual si es necesario
+source /path/to/venv/bin/activate
+
 pip install -r requirements.txt
 gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app
